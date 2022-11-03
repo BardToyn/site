175bcce559317fb50250eac4d9c14638523fb07e
BardToyn <54434924+BardToyn@users.noreply.github.com>
2022-11-03 19:19:30 +0300

    dev/ver1.0

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+//modal share
+
+let openModal = document.getElementById('OpenShare');
+let modalWrap = document.getElementById('wrapperModal');
+let modal = document.getElementById('shareModal');
+let closeModal = document.getElementById('closeShare');
+
+openModal.addEventListener('click', function () {
+    modalWrap.style.display="flex";
+    modal.classList.add('b-show');
+})
+
+closeModal.addEventListener('click', function () {
+    modalWrap.style.display="none";
+})
+
+//copy button
+
+function copyBtn() {
+    let copyText = document.getElementById("shareInput");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+//burger menu
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
